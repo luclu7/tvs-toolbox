627c8a6551998134c709e4b1f4c136dde0b10bb4
Luclu7 <me@luclu7.fr>
2021-10-02 11:10:11 +0200

    welp ça marche mieux

--- utils/index.js
+++ utils/index.js
@@ -7,9 +7,12 @@ const dataset = require("./referentiel-gares-voyageurs.json");
 
 for(let key in dataset) {
   if (/[A-Z]{3}/.test(dataset[key].fields.tvs)){
-    console.log(dataset[key].fields.gare_alias_libelle_noncontraint)
-    dict[dataset[key].fields.gare_alias_libelle_noncontraint]=dataset[key].fields;
-    listeDesGares.push(dataset[key].fields.gare_alias_libelle_noncontraint);
+    // console.log(dataset[key].fields.gare_alias_libelle_noncontraint)
+    if(dataset[key].fields.gare_alias_libelle_noncontraint === "Paris Montparnasse") {
+    	console.log(dataset[key])
+    }
+	  dict[dataset[key].fields.alias_libelle_noncontraint]=dataset[key].fields;
+    listeDesGares.push(dataset[key].fields.alias_libelle_noncontraint);
   }
 }
 // test avec saint-leu
